0240077d679e58a720f0bdf6f5366f3fc763cfa9
Amar Waghmare <amarwaghmare573@gmail.com>
2023-03-01 20:59:09 +0530

    Basic Task commit

---
+++ TestProject/src/testPAckage/TestClass.java
@@ -0,0 +1,14 @@
+package testPAckage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Testbase class");
+		System.out.println("Util class");
+		System.out.println("pagelayer class");
+		System.out.println("testlayer class");
+
+	}
+
+}
